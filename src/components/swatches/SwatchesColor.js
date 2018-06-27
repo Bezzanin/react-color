b822dbda8e5e74caf8440cb957a0cc680388c095
Bezzanin <shoxy95@mail.ru>
2018-06-27 13:40:47 +0300

    Swatches Card Custom Size

--- src/components/swatches/SwatchesColor.js
+++ src/components/swatches/SwatchesColor.js
@@ -1,78 +1,111 @@
-import React from 'react'
-import reactCSS from 'reactcss'
-import colorUtils from '../../helpers/color'
+'use strict';
 
-import { Swatch } from '../common'
-import CheckIcon from '@icons/material/CheckIcon'
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SwatchesColor = undefined;
 
-export const SwatchesColor = ({ color, onClick = () => {}, onSwatchHover, first,
-  last, active }) => {
-  const styles = reactCSS({
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactcss = require('reactcss');
+
+var _reactcss2 = _interopRequireDefault(_reactcss);
+
+var _color = require('../../helpers/color');
+
+var _color2 = _interopRequireDefault(_color);
+
+var _common = require('../common');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SwatchesColor = exports.SwatchesColor = function SwatchesColor(_ref) {
+  var color = _ref.color,
+      _ref$onClick = _ref.onClick,
+      onClick = _ref$onClick === undefined ? function () {} : _ref$onClick,
+      onSwatchHover = _ref.onSwatchHover,
+      first = _ref.first,
+      last = _ref.last,
+      active = _ref.active;
+
+  var styles = (0, _reactcss2.default)({
     'default': {
       color: {
-        width: '40px',
-        height: '24px',
+        width: '100%',
+        height: '100%',
         cursor: 'pointer',
         background: color,
         marginBottom: '1px',
+        display: 'flex',
+        alignItems: 'flex-end'
       },
       check: {
-        color: colorUtils.getContrastingColor(color),
+        fill: _color2.default.getContrastingColor(color),
         marginLeft: '8px',
-        display: 'none',
-      },
+        display: 'none'
+      }
     },
     'first': {
       color: {
         overflow: 'hidden',
-        borderRadius: '2px 2px 0 0',
-      },
+        borderRadius: '2px 2px 0 0'
+      }
     },
     'last': {
       color: {
         overflow: 'hidden',
-        borderRadius: '0 0 2px 2px',
-      },
+        borderRadius: '0 0 2px 2px'
+      }
     },
     'active': {
       check: {
-        display: 'block',
-      },
+        display: 'block'
+      }
     },
     'color-#FFFFFF': {
       color: {
-        boxShadow: 'inset 0 0 0 1px #ddd',
+        boxShadow: 'inset 0 0 0 1px #ddd'
       },
       check: {
-        color: '#333',
-      },
+        fill: '#333'
+      }
     },
     'transparent': {
       check: {
-        color: '#333',
-      },
-    },
+        fill: '#333'
+      }
+    }
   }, {
-    first,
-    last,
-    active,
+    first: first,
+    last: last,
+    active: active,
     'color-#FFFFFF': color === '#FFFFFF',
-    'transparent': color === 'transparent',
-  })
+    'transparent': color === 'transparent'
+  });
 
-  return (
-    <Swatch
-      color={ color }
-      style={ styles.color }
-      onClick={ onClick }
-      onHover={ onSwatchHover }
-      focusStyle={{ boxShadow: `0 0 4px ${ color }` }}
-    >
-      <div style={ styles.check }>
-        <CheckIcon />
-      </div>
-    </Swatch>
+  return _react2.default.createElement(
+    _common.Swatch,
+    {
+      color: color,
+      style: styles.color,
+      onClick: onClick,
+      onHover: onSwatchHover,
+      focusStyle: { boxShadow: '0 0 4px ' + color }
+    },
+    _react2.default.createElement('span', { className: 'color-name'}, `${color}`,
+    _react2.default.createElement(
+      'div',
+      { style: styles.check },
+      _react2.default.createElement(
+        'svg',
+        { style: { width: '24px', height: '24px' }, viewBox: '0 0 24 24' },
+        _react2.default.createElement('path', { d: 'M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z' })
+      )
+    )
   )
-}
+  );
+};
 
-export default SwatchesColor
+exports.default = SwatchesColor;
--- src/components/swatches/SwatchesGroup.js
+++ src/components/swatches/SwatchesGroup.js
@@ -1,36 +1,61 @@
-import React from 'react'
-import reactCSS from 'reactcss'
-import map from 'lodash/map'
+'use strict';
 
-import SwatchesColor from './SwatchesColor'
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SwatchesGroup = undefined;
 
-export const SwatchesGroup = ({ onClick, onSwatchHover, group, active }) => {
-  const styles = reactCSS({
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactcss = require('reactcss');
+
+var _reactcss2 = _interopRequireDefault(_reactcss);
+
+var _map = require('lodash/map');
+
+var _map2 = _interopRequireDefault(_map);
+
+var _SwatchesColor = require('./SwatchesColor');
+
+var _SwatchesColor2 = _interopRequireDefault(_SwatchesColor);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SwatchesGroup = exports.SwatchesGroup = function SwatchesGroup(_ref) {
+  var onClick = _ref.onClick,
+      onSwatchHover = _ref.onSwatchHover,
+      group = _ref.group,
+      active = _ref.active;
+
+  var styles = (0, _reactcss2.default)({
     'default': {
       group: {
         paddingBottom: '10px',
-        width: '40px',
+        width: '100px',
+        height: '200px',
         float: 'left',
-        marginRight: '10px',
-      },
-    },
-  })
+        marginRight: '10px'
+      }
+    }
+  });
 
-  return (
-    <div style={ styles.group }>
-      { map(group, (color, i) => (
-        <SwatchesColor
-          key={ color }
-          color={ color }
-          active={ color.toLowerCase() === active }
-          first={ i === 0 }
-          last={ i === group.length - 1 }
-          onClick={ onClick }
-          onSwatchHover={ onSwatchHover }
-        />
-      )) }
-    </div>
-  )
-}
+  return _react2.default.createElement(
+    'div',
+    { style: styles.group },
+    (0, _map2.default)(group, function (color, i) {
+      return _react2.default.createElement(_SwatchesColor2.default, {
+        key: color,
+        color: color,
+        active: color.toLowerCase() === active,
+        first: i === 0,
+        last: i === group.length - 1,
+        onClick: onClick,
+        onSwatchHover: onSwatchHover
+      });
+    })
+  );
+};
 
-export default SwatchesGroup
+exports.default = SwatchesGroup;
